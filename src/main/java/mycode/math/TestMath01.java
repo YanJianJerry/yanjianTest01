14f45a39d1f5965272ad969ec57cb8821e4105aa
yanjian <yanjian@flux.com.cn>
2024-06-13 16:02:35 +0800

    20240613

---
+++ src/main/java/mycode/math/TestMath01.java
@@ -0,0 +1,22 @@
+package mycode.math;
+
+/**
+ * @author yanjian
+ */
+public class TestMath01 {
+    public static void main(String[] args) {
+        //
+        test01();
+    }
+
+    /**
+     * 浮点数取模运算
+     */
+    public static void test01(){
+        double qty1= 10.3;
+        double result = qty1%5;
+        System.out.println("取模后的值为："+result);
+    }
+
+    //
+}
