4b2e7c4d6243723fda263c9843b90d8778eb5327
yanjian <yanjian@flux.com.cn>
2024-08-01 19:19:28 +0800

    20240801

--- src/com/object/testArrays/TestArray01.java
+++ src/com/object/testArrays/TestArray01.java
@@ -1,9 +1,15 @@
-package object.Arrays;
+package com.object.testArrays;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestArray01 {
     public static void main(String[] args) {
         //
-        test01();
+        //test01();
+
+        //
+        test02();
     }
 
     public static void test01(){
@@ -11,5 +17,30 @@ public class TestArray01 {
         System.out.println(strArray01[0]);
         strArray01 = new String[]{"1111"};
         System.out.println(strArray01[0]);
+    }
+
+
+    /**
+     * 数组赋值测试
+     */
+    private static void test02(){
+        List<String[]> list1 = new ArrayList<>();
+        String[] arr1 = {"aaa","bbb"};
+        String[] arr2 = new String[3];
+        int[] arr3 = new int[]{1,3,4};
+
+        //
+        list1.add(arr1);
+        System.out.println(list1);
+
+        //
+        arr1 = new String[]{"111","2222"};
+        list1.add(arr1);
+        System.out.print(list1);System.out.println(list1.get(0)[0]);
+
+        //
+        list1.get(0)[0] = "AAAAAAAAAA";
+        System.out.print(list1);System.out.println(list1.get(0)[0]);
+
     }
 }
